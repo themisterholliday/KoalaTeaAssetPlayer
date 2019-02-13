798d9ad411889dd8b6d4c441ac5c02966f493861
Craig Holliday <themisterholliday@gmail.com>
2019-02-13 11:52:46 -0600

    Re: fix added file issues

--- Example/Tests/VideoExporterSpec.swift
+++ Example/Tests/VideoExporterSpec.swift
@@ -33,14 +33,14 @@ class VideoExporterSpec: QuickSpec {
             afterEach {
                 if let url = fileUrl {
                     // Remove this line to manually review exported videos
-                    FileHelpers.removeFileAtURL(fileURL: url)
+//                    FileHelpers.removeFileAtURL(fileURL: url)
                 }
 
                 fileUrl = nil
                 progressToCheck = 0
             }
 
-            context("export video with theme preset") {
+            context("export video") {
                 it("should complete export with progress") {
                     let start = Date()
 
@@ -71,6 +71,70 @@ class VideoExporterSpec: QuickSpec {
                     expect(firstVideoTrack?.naturalSize.width).to(equal(1280))
                     expect(firstVideoTrack?.naturalSize.height).to(equal(720))
                     expect(firstVideoTrack?.asset?.duration.seconds).to(equal(5))
+                }
+            }
+
+            context("export video with watermark") {
+                fit("should complete export with progress") {
+                    let start = Date()
+
+                    let finalAsset = thirtySecondAsset.changeStartTime(to: 0.0).changeEndTime(to: 5.0)
+
+                    let watermarkView = UIImageView(frame: CGRect(x: 0, y: 0, width: 200, height: 100))
+                    watermarkView.contentMode = .scaleAspectFit
+                    watermarkView.image = UIImage(named: "long_story_watermark")
+                    watermarkView.layer.rasterizationScale = 2.0
+                    watermarkView.layer.contentsScale = 2.0
+                    watermarkView.layer.shouldRasterize = true
+
+                    VideoExporter
+                        .exportVideoWithoutCrop(videoAsset: finalAsset,
+                                                watermarkView: watermarkView,
+                                                progress:
+                            { (progress) in
+                                print("export video: \(progress)")
+                                progressToCheck = progress
+                        }, success: { returnedFileUrl in
+                            print(returnedFileUrl, "exported file url")
+                            fileUrl = returnedFileUrl
+
+                            print(Date().timeIntervalSince(start), "<- End Time For Export")
+                        }, failure: { (error) in
+                            expect(error).to(beNil())
+                            fail()
+                        })
+
+                    expect(progressToCheck).toEventually(beGreaterThan(0.5), timeout: 30)
+                    expect(fileUrl).toEventuallyNot(beNil(), timeout: 30)
+
+                    // Check just saved local video
+                    let savedVideo = VideoAsset(url: fileUrl!)
+                    let firstVideoTrack = savedVideo.urlAsset.getFirstVideoTrack()
+                    expect(firstVideoTrack?.naturalSize.width).to(equal(1280))
+                    expect(firstVideoTrack?.naturalSize.height).to(equal(720))
+                    expect(firstVideoTrack?.asset?.duration.seconds).to(equal(5))
+                }
+            }
+
+            context("media watermark test") {
+                it("") {
+                    let logoImage = UIImage(named: "long_story_watermarkx200")
+                    let item = MediaItem(url: thirtySecondAsset.urlAsset.url)!
+
+                    let firstElement = MediaElement(image: logoImage!)
+                    firstElement.frame = CGRect(x: 0, y: 0, width: 200, height: 100)
+
+                    item.add(elements: [firstElement])
+
+                    var url: URL?
+
+                    let mediaProcessor = MediaProcessor()
+                    mediaProcessor.processElements(item: item) { [weak self] (result, error) in
+                        print(result.processedUrl)
+                        url = result.processedUrl
+                    }
+
+                    expect(url).toEventuallyNot(beNil(), timeout: 180)
                 }
             }
 
